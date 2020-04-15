6bcf8a4ed93a9e82545dca7cab0f588031d05aed
Parsa Mastouri <pastour@Parsas-MacBook-Pro.local>
2020-04-15 18:12:18 +0430

    added shop page, collection preview component & collection item component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/Homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import './App.css'
 
 
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shops' component={ShopPage} />
       </Switch>
     </div>
   );
