fa72892edbb2868d332ab4b892a388c4a6a9d0dc
Parsa Mastouri <pastour@Parsas-MacBook-Pro.local>
2020-04-23 13:59:45 +0430

    updateing our application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon } from './../../assets/shopping-bag.svg';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -17,5 +18,23 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+/******** THIS SHIT IS CALLED A SELECTOR
+it will call everytime anything changes in our state so it's a perfomance unreliability
+SOLUTION:
+    caching (memoization) & using re-selector library
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems }}) => {
+    console.log('I am being called');
+    return{
+        itemCount: cartItems.reduce((accumlatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+    }
+}
+*********/
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
