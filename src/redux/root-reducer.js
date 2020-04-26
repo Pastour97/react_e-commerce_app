9b1f3f2c3835b6d38d408576d9b6978ffe0b2b3d
Pastour97 <parsa.pmk.code@gmail.com>
2020-04-26 16:49:43 +0430

    added redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //this line tells redux-persist to use localStorage as default storage
 
 import  userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',              //it tells at what point we want to store stuff
+    storage,                 //it gives storage key to the storage we imported
+    whitelist: ['cart']     //an array containing
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,29 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+
 
  import rootReducer from './root-reducer';
 
  const middlewares = [logger];
 
- const store = createStore(rootReducer, applyMiddleware(...middlewares));
+ //export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+ /////////////////////////////////////////////////////////////
+//use the createStore above if the project is over 
+ const composeEnhancers =
+ typeof window === 'object' &&
+ window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+     // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+   }) : compose;
+
+const enhancer = composeEnhancers(
+ applyMiddleware(...middlewares),
+ // other store enhancers if any
+);
+export const store = createStore(rootReducer, enhancer);
+//////////////////////////////////////////////////////////////
+
+export const persistor = persistStore(store);
 
 
- export default store;
