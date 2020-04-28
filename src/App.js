59c3eb648f79871442160ee8cf84a95e902eaad2
Pastour97 <parsa.pmk.code@gmail.com>
2020-04-28 05:07:23 +0430

    moved our directory aand shop data into its own reducers, created respective selectors & updated corresponding components with new redux flow for directory/shop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -56,16 +56,16 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
-          <Route 
-            exact 
-            path='/signin' 
-            render={() => 
+          <Route
+            exact
+            path='/signin'
+            render={() =>
               this.props.currentUser ? (
                 <Redirect to='/' />
               ) : (
                 <SignInAndSignUp />
               )
-            } 
+            }
           />
         </Switch>
       </div>
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,16 +6,15 @@ import './collection-preview.styles.scss';
 
 
 
-
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-         {items
-         .filter( (item, index) => index < 4)
-         .map( (item) => (
-             <CollectionItem key={item.id} item={item}></CollectionItem>
-         ))}
+           {items
+            .filter( (_item, index) => index < 4)
+            .map( (item) => (
+                <CollectionItem key={item.id} item={item}></CollectionItem>
+            ))}
         </div>
     </div>
 )
